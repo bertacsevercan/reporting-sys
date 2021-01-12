f51473d4454220325fcc00756f490a7fca2fb50d
bertacsevercan <bertac.severcan@gmail.com>
2021-01-13 00:09:56 +0300

    add redux and actions and services

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import 'antd/dist/antd.css';
-import './App.css';
+import "antd/dist/antd.css";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
