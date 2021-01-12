bad89d06c34c7beda0dd5ad6360dedb88a17e97a
bertacsevercan <bertac.severcan@gmail.com>
2021-01-13 02:32:53 +0300

    add register and login compontents

--- front-end/src/components/Login/index.jsx
+++ front-end/src/components/Login/index.jsx
@@ -1,59 +1,113 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { login } from "../actions/auth";
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
+import { Form, Input, Button, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
 
 const Login = (props) => {
-  const form = useRef();
-  const checkBtn = useRef();
-
-  const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
-  const [loading, setLoading] = useState(false);
-
   const { isLoggedIn } = useSelector((state) => state.auth);
   const { message } = useSelector((state) => state.message);
 
   const dispatch = useDispatch();
 
-  const handleChange = (e) => {
-    setCredentials({
-      ...credentials,
-      [e.target.name]: e.target.value,
-    });
+  const handleLogin = (values) => {
+    dispatch(login(values.username, values.password))
+      .then(() => {
+        props.history.push("/profile");
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    setLoading(true);
-
-    form.current.validateAll();
-
-    if (checkBtn.current.context._errors.length === 0) {
-      dispatch(login(credentials.username, credentials.password))
-        .then(() => {
-          props.history.push("/profile");
-          window.location.reload();
-        })
-        .catch(() => {
-          setLoading(false);
-        });
-    } else {
-      setLoading(false);
-    }
+  const handleLoginFailed = (errorInfo) => {
+    console.log("Login failed:", errorInfo);
   };
 
   if (isLoggedIn) {
     return <Redirect to="/profile" />;
   }
 
-  return <div className="login"></div>;
+  return (
+    <div className="login">
+      <div className="avatar-wrapper">
+        <Avatar
+          size={{
+            xs: 24,
+            sm: 32,
+            md: 40,
+            lg: 64,
+            xl: 80,
+            xxl: 100,
+          }}
+          icon={<UserOutlined />}
+        />
+      </div>
+      <Form
+        {...layout}
+        name="loginForm"
+        onFinish={handleLogin}
+        onFinishFailed={handleLoginFailed}
+        initialValues={{ remember: true }}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+
+        {message && (
+          <div className="alert danger" role="alert"> {/* change them to antd alert */}
+            {message}
+          </div>
+        )}
+      </Form>
+    </div>
+  );
 };
 
 export default Login;
---
+++ front-end/src/components/Register/index.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../actions/auth";
+import { Form, Input, Button, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+const Register = () => {
+  const [successful, setSuccessful] = useState(false);
+  const { message } = useSelector((state) => state.message);
+  const dispatch = useDispatch();
+
+  const handleRegister = (values) => {
+    dispatch(register(values.username, values.email, values.password))
+      .then(() => {
+        setSuccessful(true);
+      })
+      .catch(() => {
+        setSuccessful(false);
+      });
+  };
+
+  const handleRegisterFailed = (err) => {
+    console.log("Register failed:", err);
+  };
+
+  return (
+    <div className="register">
+      <div className="avatar-wrapper">
+        <Avatar
+          size={{
+            xs: 24,
+            sm: 32,
+            md: 40,
+            lg: 64,
+            xl: 80,
+            xxl: 100,
+          }}
+          icon={<UserOutlined />}
+        />
+      </div>
+      <Form
+        {...layout}
+        name="registerForm"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={handleRegister}
+        onFinishFailed={handleRegisterFailed}
+      >
+        {!successful && (
+          <>
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              name="email"
+              label="E-mail"
+              rules={[
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!",
+                },
+                {
+                  required: true,
+                  message: "Please input your E-mail!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </>
+        )}
+        {message && (
+          <div
+            className={
+              successful ? "alert alert-success" : "alert alert-danger" // change these to antd alert
+            }
+            role="alert"
+          >
+            {message}
+          </div>
+        )}
+      </Form>
+    </div>
+  );
+};
+
+export default Register;
