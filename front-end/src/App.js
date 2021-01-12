5cca690ef958a41f8b0aadd6f7641e7ce042c330
bertacsevercan <bertac.severcan@gmail.com>
2021-01-12 23:46:13 +0300

    create react app and add dependecies

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'antd/dist/antd.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
