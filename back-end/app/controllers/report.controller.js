7c12fb19c750123afca528043722f09f99ab0c75
bertacsevercan <bertac.severcan@gmail.com>
2021-01-17 21:28:27 +0300

    add new api

--- back-end/app/controllers/report.controller.js
+++ back-end/app/controllers/report.controller.js
@@ -1,9 +1,12 @@
 const db = require("../models");
+const { QueryTypes } = require("sequelize");
 
 const User = db.user;
 const Report = db.report;
-
+const sq = db.sequelize;
 const Op = db.Sequelize.Op;
+const date = new Date();
+const currentYear = date.getFullYear();
 
 exports.createReport = (req, res) => {
   Report.create({
@@ -32,16 +35,241 @@ exports.createReport = (req, res) => {
 
 exports.findAll = (req, res) => {
   const userId = req.query.userId;
-  var condition = userId ? { userId: { [Op.like]: userId } } : null;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
 
-  Report.findAll({ where: condition })
+  Report.findAll({
+    where: condition,
+  })
     .then((data) => {
       res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving tutorials.",
+        message: err.message || "Some error occurred while retrieving reports.",
+      });
+    });
+};
+
+exports.locationTotal = (req, res) => {
+  const userId = req.query.userId;
+  const saleType = req.query.type;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
+
+  Report.findAll({
+    attributes: [
+      "userId",
+      [sq.fn("SUM", sq.col("saleAmount")), "total"],
+      "location",
+    ],
+    where: {
+      [Op.and]: [{ saleType: { [Op.eq]: saleType } }, condition],
+    },
+    group: ["location"],
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving reports.",
+      });
+    });
+};
+
+/* exports.locationTotal = (req, res) => {
+  const userId = req.query.userId;
+  const column = req.query.column;
+  const saleType = req.query.type;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
+
+  Report.findAll({
+    attributes: ["userId", [sq.fn("SUM", sq.col("saleAmount")), "total_sold"]],
+    where: {
+      [Op.and]: [
+        { location: { [Op.eq]: column } },
+        { saleType: { [Op.eq]: saleType } },
+        condition,
+      ],
+    },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving reports.",
+      });
+    });
+}; */
+
+exports.estateTotal = (req, res) => {
+  const userId = req.query.userId;
+  const saleType = req.query.type;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
+
+  Report.findAll({
+    attributes: [
+      "userId",
+      [sq.fn("SUM", sq.col("saleAmount")), "total"],
+      "estateType",
+    ],
+    where: {
+      [Op.and]: [{ saleType: { [Op.eq]: saleType } }, condition],
+    },
+    group: ["estateType"],
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving reports.",
+      });
+    });
+};
+
+exports.roomTotal = (req, res) => {
+  const userId = req.query.userId;
+  const saleType = req.query.type;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
+
+  Report.findAll({
+    attributes: [
+      "userId",
+      [sq.fn("SUM", sq.col("saleAmount")), "total"],
+      "roomSize",
+    ],
+    where: {
+      [Op.and]: [{ saleType: { [Op.eq]: saleType } }, condition],
+    },
+    group: ["roomSize"],
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving reports.",
+      });
+    });
+};
+
+/* exports.estateTotal = (req, res) => {
+  const userId = req.query.userId;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
+
+  Report.findAll({
+    attributes: ["userId", [sq.fn("SUM", sq.col("saleAmount")), "total_sold", "estateType", "saleType"]],
+    where : condition ,
+    group : ["estateType", "saleType"]
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving reports.",
+      });
+    });
+};
+
+
+exports.roomTotal = (req, res) => {
+  const userId = req.query.userId;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
+
+  Report.findAll({
+    attributes: ["userId", [sq.fn("SUM", sq.col("saleAmount")), "total_sold", "roomSize", "saleType"]],
+    where : condition,
+    group : ["roomSize", "saleType"]
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving reports.",
+      });
+    });
+};
+
+
+ */
+
+exports.dateTotal = (req, res) => {
+  const userId = req.query.userId;
+  const saleType = req.query.type;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
+
+  Report.findAll({
+    attributes: [
+      "userId",
+      [sq.fn("SUM", sq.col("saleAmount")), "total"],
+      "time",
+    ],
+    where: {
+      [Op.and]: [{ saleType: { [Op.eq]: saleType } }, condition],
+    },
+    group: ["time"],
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving reports.",
+      });
+    });
+};
+
+exports.yearlyTotal = (req, res) => {
+  const userId = req.query.userId;
+  const column = req.query.column;
+  const saleType = req.query.type;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
+
+  Report.findAll({
+    attributes: ["userId", [sq.fn("SUM", sq.col("saleAmount")), "total_sold"]],
+    where: {
+      [Op.and]: [
+        { time: { [Op.startsWith]: column } },
+        { saleType: { [Op.eq]: saleType } },
+        condition,
+      ],
+    },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving reports.",
+      });
+    });
+};
+
+exports.monthlyTotal = (req, res) => {
+  const userId = req.query.userId;
+  const column = req.query.column;
+  const saleType = req.query.type;
+  var condition = userId ? { userId: { [Op.eq]: userId } } : null;
+
+  Report.findAll({
+    attributes: ["userId", [sq.fn("SUM", sq.col("saleAmount")), "total_sold"]],
+    where: {
+      [Op.and]: [
+        { time: { [Op.eq]: currentYear + "-" + column } },
+        { saleType: { [Op.eq]: saleType } },
+        condition,
+      ],
+    },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving reports.",
       });
     });
 };
--- back-end/app/routes/report.routes.js
+++ back-end/app/routes/report.routes.js
@@ -1,4 +1,5 @@
 const controller = require("../controllers/report.controller");
+const { authJwt } = require("../middleware");
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
@@ -9,6 +10,16 @@ module.exports = function (app) {
     next();
   });
 
-  app.post("/api/report", controller.createReport);
-  app.get("/api/report", controller.findAll);
+  app.post("/api/report", controller.createReport, [authJwt.verifyToken]);
+  app.get("/api/report", controller.findAll, [authJwt.verifyToken]);
+  app.get("/api/report/location", controller.locationTotal, [
+    authJwt.verifyToken,
+  ]);
+  app.get("/api/report/estate", controller.estateTotal, [authJwt.verifyToken]);
+  app.get("/api/report/rooms", controller.roomTotal, [authJwt.verifyToken]);
+  app.get("/api/report/date", controller.roomTotal, [authJwt.verifyToken]);
+  /*  app.get("/api/report/yearly", controller.yearlyTotal, [authJwt.verifyToken]);
+  app.get("/api/report/monthly", controller.monthlyTotal, [
+    authJwt.verifyToken,
+  ]); */
 };
